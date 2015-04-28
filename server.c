ca956fb0e94cc8aa9745d19f3ebe5fa53fb51252
Luca Coviello <luca.coviello@gmail.com>
2015-04-28 10:36:08 +0200

    parsing server parameters

--- server.c
+++ server.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <getopt.h>
 
 
 #define MAX_BUF 1024
@@ -16,6 +17,9 @@
 #define SERVER_PATH "/tmp/hairy-happiness"
 #define DELIM " "
 #define MAX_PLAYERS 10
+#define MIN_PLAYERS 0
+#define MAX_POINTS 100
+#define MIN_POINTS 10
 
 
 typedef struct {
@@ -36,33 +40,34 @@ int new_question(char * q) {
 
 int answer(char buf[MAX_BUF], char * strtok_ctx, player* players) {
     // TODO write me
+    return -1;
 }
 
 
-void join(char buf[MAX_BUF], char * strtok_ctx, player* players,  int &n_players) {
+void join(char buf[MAX_BUF], char * strtok_ctx, player* players,  int*active_players, int n_players) {
 
     player pl;
     // open output FIFO to the client
     char * fifo_path = strtok_r(NULL, DELIM, &strtok_ctx);
     pl.fifo = open(fifo_path, O_WRONLY);
 
-    if (n_players < MAX_PLAYERS) { 
+    if (*active_players < n_players) { 
         // the player is accepted 
 
         // get the nickname of the new player
         pl.name = strtok_r(NULL, DELIM, &strtok_ctx);
 
         // calculate the point for the new client
-        pl.pt = MAX_PLAYERS - n_players - 1;
+        pl.pt = n_players - *active_players - 1;
 
         char msg[MAX_BUF];
-        sprintf(msg, "accepted %d", n_players);
+        sprintf(msg, "accepted %d", *active_players);
         write(pl.fifo, msg, MAX_BUF);
 
         // TODO send the question
 
         // add the client to the player list
-        players[n_players++] = pl;
+        players[*active_players++] = pl;
     }
     else {
         // request is rejected
@@ -75,14 +80,62 @@ void join(char buf[MAX_BUF], char * strtok_ctx, player* players,  int &n_players
 }
 
 
-int main() {
+int parse_args(int argc, char *argv[], int *n_players, int *points_to_win) {
+    int option_index = 0;
+    int c;
+    int n_flag = 0;
+    int p_flag = 0;
+
+    static struct option long_options[] = {
+            {"max",  required_argument, 0,  'm' },
+            {"win",  required_argument, 0,  'w' }
+    };
+
+    while ((c = getopt_long(argc, argv,"m:w:", 
+                   long_options, &option_index )) != -1) {
+        int tmp;
+        switch (c) {
+            case 'm' :
+                n_flag = 1;
+                tmp = atoi(optarg);
+                if (tmp >= MIN_PLAYERS && tmp <= MAX_PLAYERS) {
+                    *n_players = tmp;
+                }
+                else {
+                    printf("--max argument must be between %d and %d, using %d\n", MIN_PLAYERS, MAX_PLAYERS, MAX_PLAYERS);
+                    *n_players = MAX_PLAYERS;
+                }
+            break;
+            case 'w' :
+                p_flag = 1;
+                tmp = atoi(optarg);
+                if (tmp >= MIN_POINTS && tmp <= MAX_POINTS) {
+                    *points_to_win = tmp;
+                }
+                else {
+                    printf("--win argument must be between %d and %d, using %d\n", MIN_POINTS, MAX_POINTS, MAX_POINTS);
+                    *points_to_win = MAX_POINTS;
+                }
+                break;
+            }
+    }
+
+    return n_flag && p_flag;
+}
+
+
+int main(int argc, char *argv[]) {
+    int points_to_win, n_players;
+    if (!parse_args(argc, argv, &n_players, &points_to_win)) {
+        printf("Usage: ./sever --max <maxplayers> --win <winningpoint\n");
+    }
+
     srand(time(NULL));
 
-    int in_f;
     char buf[MAX_BUF];
     player players[MAX_PLAYERS];
-    int n_players = 0;
-    char[8] question;
+    int active_players = 0;
+    char question[8];
     int res = new_question(question);
 
     // TODO check for other servers running
@@ -92,7 +145,7 @@ int main() {
     mkfifo(SERVER_PATH, 0666);
 
     // open the input FIFO with read and write permissions to avoid blocks
-    in_f = open(SERVER_PATH, O_RDWR);
+    int in_f = open(SERVER_PATH, O_RDWR);
 
     while (1) {
         // read incoming messages from the FIFO
@@ -108,7 +161,7 @@ int main() {
             // a new client requested to join
 
             // TODO check if buf is edited in debian
-            join(buf, strtok_ctx, players, n_players);
+            join(buf, strtok_ctx, players, &active_players, n_players);
         } 
         else if (!strcmp(op, "try")) {
             // received answer
