e63251a1ee5b9bda0ea0ad0adbd4b59654e784c6
alefesti <ale.festi@icloud.com>
2015-05-04 12:45:38 +0200

    Update client.c
    CASACSCA

--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 */
 
 #include <fcntl.h>
+#include <stdarg.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -30,156 +31,155 @@
 #define MSG_QUIT "quit"
 
 
-/*
-    struct for passing argumnets in pthread_create
-*/
-typedef struct thread_args {
-    int in_f;
-} thread_args;
+#define ERR_NOTACCSF -1
+#define ERR_NOTACCAE -2
 
-
-/*
-    send join request to server
-*/
-void join_game(int out_f, char * name, char * client_path) {
-    char buf[MAX_BUF];
-    sprintf(buf, "%s%s%s%s%s", MSG_JOIN, DELIM, client_path, DELIM, name);
-    write(out_f, buf, strlen(buf));
-    
+char * toString ( const char * format, ... )
+{
+    char buffer[MAX_BUF];
+    va_list args;
+    va_start (args, format);
+    vsprintf (buffer,format, args);
+    //perror (buffer);
+    va_end (args);
+    return strdup(buffer);
 }
 
 
 /* 
     manage the user interaction with the game
 */
-void read_fifo(void* args) {
-    thread_args c = *((thread_args*) args);
+void read_fifo(void* args)
+{
+    int recvFIFO = *((int*) args);
 
-    while(1) {
+    while(1)
+    {
         // read message
         char buf[MAX_BUF];
-        read(c.in_f, buf, MAX_BUF);
+        read(recvFIFO, buf, MAX_BUF);
 
         // extract the op
-        char * buf_dup = strdup(buf);
-        char * op = strtok(buf, DELIM);
-        if (!strcmp(op, MSG_QUESTION)) {
-            // obtain only the question
-            buf_dup += 2;
-            write(1, buf_dup, strlen(buf_dup));
+        char * data;
+        char * op = strtok_r(buf, DELIM, &data);
+        if (!strcmp(op, MSG_QUESTION))
+        {
+            write(1, strtok_r(NULL, DELIM, &data), MAX_BUF);
         }
-        else if (!strcmp(op, MSG_CORRECT)) {
+        else if (!strcmp(op, MSG_CORRECT))
+        {
             printf("correct\n");
         }
-        else if (!strcmp(op, MSG_INCORRECT)) {
+        else if (!strcmp(op, MSG_INCORRECT))
+        {
             printf("wrong\n");
         }
-        else if (!strcmp(op, MSG_END)) {
+        else if (!strcmp(op, MSG_END))
+        {
             printf("end\n");
         }
-        else if (!strcmp(op, MSG_QUIT)) {
+        else if (!strcmp(op, MSG_QUIT))
+        {
             printf("someone has quit\n");
+        }
+        else if (!strcmp(op, MSG_JOIN))
+        {
+            printf("someone joined\n");
         }
     }
 }
 
 
-void read_console(int out_f, int index) {
-    while (1) {
+void read_console(int sendFIFO, int index)
+{
+    while (1)
+    {
         // wait for the user to answer
-        long answer;
-        char buf[MAX_BUF];
         char input[MAX_BUF];
-
         size_t len = read(0, input, MAX_BUF);
         input[len] = '\0';
 
-        if (!strcmp(input, "quit\n")) {
-            sprintf(buf, "%s%s%d", MSG_QUIT, DELIM, index);
-            write(out_f, buf, strlen(buf));
-            exit(0);
+        if (!strcmp(input, "quit\n"))
+        {
+            write(sendFIFO, toString("%s%s%d", MSG_QUIT, DELIM, index), MAX_BUF);
+            break;
         }
-        else {
+        else
+        {
             errno = 0;
             // convert the number
-            answer = strtol(input, NULL, 10);
+            long answer = strtol(input, NULL, 10);
 
-            if (errno != 0 && answer == 0) {
+            if (errno != 0 && answer == 0)
+            {
                 // conversion error
-                // TODO what about mutex?
                 printf("Insert a number please\n");
             }
-            else {
-                // send the user answer
-                sprintf(buf, "%s%s%d%s%ld", MSG_ANSWER, DELIM, index, DELIM, answer);
-                write(out_f, buf, strlen(buf));
+            else // send the user answer
+            {
+                write(sendFIFO, toString("%s%s%d%s%ld", MSG_ANSWER, DELIM, index, DELIM, answer), MAX_BUF);
             }
         }
     }
 }
 
 
-int main() {
-    char client_path[20] = "/tmp/";
-    char buf[MAX_BUF];
-    char name[NAME_LEN];
 
-    // generate fifo name using timestamp
-    // FIXME: timestamp has seconds precision
-    int ts = (unsigned)time(NULL);    
-    sprintf(client_path, "%s%u", client_path, ts);
-    // printf("%s\n", client_path);
-
+int main()
+{
     // get user nickname
+    char name[NAME_LEN];
     printf("Enter your nickname: ");
     scanf("%10s", name);
     
-    // create the FIFO 
-    mkfifo(client_path, 0666);
 
     // open the output FIFO with write permissions and wait for the server to exist
-    int out_f = open(SERVER_PATH, O_WRONLY);
-
-    join_game(out_f, name, client_path);
-
-    // open the input FIFO with read permissions (blocking)
-    int in_f = open(client_path, O_RDONLY);
+    int sendFIFO = open(SERVER_PATH, O_WRONLY);
+    
+    
+    char client_path[30];
+    // generate fifo name using timestamp  FIXME: timestamp has seconds precision
+    sprintf(client_path, "/tmp/%u", (unsigned)time(NULL));
+    // create the FIFO
+    mkfifo(client_path, 0666);    // open the input FIFO with read permissions (blocking)
+    int recvFIFO = open(client_path, O_RDWR);
+    
+    
+    
+    //write join request
+    write(sendFIFO, toString("%s%s%s%s%s", MSG_JOIN, DELIM, client_path, DELIM, name), MAX_BUF);
     // read the answer to the join request
-    size_t len = read(in_f, buf, MAX_BUF);
+    char buf[MAX_BUF];
+    size_t len = read(recvFIFO, buf, MAX_BUF);
     buf[len] = '\0';
 
-    // TODO use strtok to undertan the response
-    int index;
     char * op = strtok(buf, DELIM);
-    if (strcmp(op, MSG_ACCEPTED))
+    
+    if (!strcmp(op, MSG_NOTACCEPTED))
     {
-        printf("The server didn't accepted you :(\n");
+        int err = atoi(strtok(NULL, DELIM));
+        if(err == ERR_NOTACCAE)
+            printf("The server didn't accepted you, your name already exist\n");
+        else if (err == ERR_NOTACCSF)
+            printf("The server is full\n");
     }
-    else
+    else if (!strcmp(op, MSG_ACCEPTED))
     {
+        printf("Joined to server\n");
+        
         // get the index of this client on the server
-        index = atoi(strtok(NULL, DELIM));
+        int index = atoi(strtok(NULL, DELIM));
         
         // create thread for listening to the server
         pthread_t thread_id;
-
-        // instance thread_args struct
-        thread_args args;
-        args.in_f = in_f;
-
-        // create the thread
-        pthread_create(&thread_id, NULL, (void*)read_fifo, (void*)&args);
+        pthread_create(&thread_id, NULL, (void*)read_fifo, (void*)&recvFIFO);
 
         // keep sending user answer
-        read_console(out_f, index);
+        read_console(sendFIFO, index);
     }
 
-
-    close(in_f);
+    close(recvFIFO);
     unlink(client_path);
 
     return 0;
-
-    // TODO close & unlink fifo(s)
-
 }
